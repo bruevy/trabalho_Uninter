aa0ab945d3804264bf8093696ccc1c50ddbb4c2b
bruevy <brualcantara12@gmail.com>
2025-07-06 20:54:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+  document.addEventListener("DOMContentLoaded", function () {
+  const botao = document.getElementById("meuBotao");
+  const mensagem = document.getElementById("mensagem");
+
+  botao.addEventListener("click", function () {
+    mensagem.textContent = "Estou realizando um sonho… Cada passo até aqui valeu a pena. Deus guiou meus caminhos, e hoje sou prova de que a fé e a persistência tornam o impossível, possível.";
+  });
+});
